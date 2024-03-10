273888eacf4398717e7f1402e87e3acd8b697762
Mvoi frank <mvoifranklin@gmail.com>
2024-03-10 14:10:01 +0000

    favicon

--- port_9/next.config.mjs
+++ port_9/next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// next.config.js
+/*module.exports = {
+  // ... other configurations
+    /*images: {
+    domains: ['your-domain.com'], // Replace with your domain if favicon hosted elsewhere
+  },*/
+/*  favicon: './public/favicon.ico', // Or replace with your favicon filename and extension
+}
+*/
