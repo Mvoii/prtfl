14eccd1406927535d9a7af6ab86a8e8cbe09e691
Mvoi frank <mvoifranklin@gmail.com>
2024-03-10 14:38:59 +0000

    almodt

--- port_9/src/app/components/Footer.jsx
+++ port_9/src/app/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span>
+          <Image 
+            src="/images/origami-logo.png"
+            alt="origami logo"
+            width={40}
+            height={30}
+          />
+        </span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
--- port_9/src/app/components/Navbar.jsx
+++ port_9/src/app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Image from "next/image";
 
 const navLinks = [
   {
@@ -30,7 +31,12 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
+          <Image 
+            src="/images/origami-logo.png"
+            alt="origami logo"
+            width={60}
+            height={20}
+          />
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
